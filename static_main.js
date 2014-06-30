ee7d83404b84578c0617a00dc5b8a2166c0bbdf3
peseus <tanjinwen.8@qq.com>
2014-07-01 00:23:08 +0800

    生成的文章页的静态页

--- static_main.js
+++ static_main.js
@@ -13,7 +13,9 @@ var _ = require('./underscore.js')._;
 var proj_path = '../peseus.github.io';
 var proj_index_template = proj_path + '/index.template.html';
 var proj_config = proj_path + '/post/index.json';
-var index_cate_tag = 'index_';
+var cate_prefix = "cat-";
+var article_dir = 'article';
+var index_cate_tag = "index";
 var index_cate_name = "首页";
 
 var proj_config_data = require(proj_config);
@@ -32,7 +34,7 @@ function createMainData() {
 	}
 	main_data.navlist = _.map(cates, function(cate) {
         return {
-            link: '/' + cate.name + '/page-1.html',
+            link: '/' + cate_prefix + cate.name + '/page-1.html',
             text: cate.text
         };
     });
@@ -50,7 +52,7 @@ function createPagebarData (cate_tag, total_page, page_num){
     for (var i = 1; i <= total_page; i++) {
         pages[i - i] = {"num" : i} ;
     }
-    pagebar_data.cate_tag = cate_tag;
+    pagebar_data.cate_tag = cate_prefix + cate_tag;
     pagebar_data.pages = pages;
     pagebar_data.num_up = page_num - 1 <= 0 ? 1 : page_num - 1;
     pagebar_data.num_next = parseInt(page_num, 10) + 1 > total_page ? total_page : parseInt(page_num, 10) + 1;
@@ -64,7 +66,7 @@ function createPersonalData () {
 function createSidebarData (cate) {
     var sidebar_data = {};
     var articles = proj_config_data.articles;
-    if(cate !== index_cate_tag) {
+    if(null !== cate && cate !== index_cate_tag) {
         articles = _.filter(articles, function(article) {
             return article.cate == cate;
         });
@@ -78,8 +80,9 @@ function createSidebarData (cate) {
         return {
             month: key,
             articles: _.map(value, function(article) {
+            	var link_addr = '/' + article_dir + article.file + '.html';
                 return {
-                    link: article.file,
+                    link: link_addr,
                     text: article.title
                 };
             })
@@ -155,17 +158,71 @@ fs.readFile(proj_index_template, 'utf8', function (error, index_template) {
 //	});
 });
 
-function createPage(cate_tag, total_page, page_num) {
+function createArticleView(article, article_info) {
+	var dst_dir = proj_path + '/' + article_dir;
+	var dst_file = proj_path + '/' + article_dir + '/' + article + '.html';
+	var article_html = article_info.html;
+	
+	var link_data = createLinkData();
+	var main_data = createMainData();
+	var personal_data = createPersonalData();
+	var sidebar_data = createSidebarData(null);
+	
+	try {
+		fs.mkdirSync(dst_dir);
+	}
+	catch (e) {
+		if (e.code !== 'EEXIST') {
+			throw e;
+		}
+	}
+	var link_html = mustache.render(my_global.index.template.link_template, link_data);
+	var main_html = mustache.render(my_global.index.template.main_template, main_data);
+	var personal_html = mustache.render(my_global.index.template.personal_template, personal_data);
+	var sidebar_html = mustache.render(my_global.index.template.sidebar_template, sidebar_data);
+	
+	// 不能直接操作原文，只能对副本进行操作
+	var document = jsdom('');
+	document.innerHTML = my_global.index.document.innerHTML;
+	var window = document.parentWindow;
+	
+	document.getElementsByClassName('main-body')[0].innerHTML = main_html;
+	document.getElementsByClassName('sidebar-nav')[0].innerHTML = sidebar_html;
+	document.getElementsByClassName('sidebar-links')[0].innerHTML = link_html;
+	document.getElementsByClassName('personal')[0].innerHTML = personal_html;
+	
+	// 插入文章内容
+	document.getElementsByClassName('article-content')[0].innerHTML = article_html;
+	
+	var dels = document.getElementsByClassName('jsdom');
+	for (var i = 0; i < dels.length; i++) {
+		dels[0].parentNode.removeChild(dels[0]);
+	}
+	var del = document.getElementById('tpl-main');
+	del.parentNode.removeChild(del);
+	del = document.getElementById('tpl-sidebar');
+	del.parentNode.removeChild(del);
+	del = document.getElementById('tpl-person');
+	del.parentNode.removeChild(del);
+	del = document.getElementById('tpl-link');
+	del.parentNode.removeChild(del);
+	del = document.getElementById('tpl-pagebar');
+	del.parentNode.removeChild(del);
+	fs.writeFileSync(dst_file, document.innerHTML);
+	console.log('写静态页面：' + dst_file + " 完成");
+}
+
+function createPageView(cate_tag, total_page, page_num) {
 	var cate_artices = my_global.cates[cate_tag];
 	var link_data = createLinkData();
 	var main_data = createMainData();
 	var pagebar_data = createPagebarData(cate_tag, total_page, page_num);
 	var personal_data = createPersonalData();
 	var sidebar_data = createSidebarData(cate_tag);
-	var dest_dir = proj_path + '/' + cate_tag;
-	var dest_path = dest_dir + '/page-' + page_num + '.html';
+	var dst_dir = proj_path + '/' + cate_prefix + cate_tag;
+	var dst_path = dst_dir + '/page-' + page_num + '.html';
 	try {
-		fs.mkdirSync(dest_dir);
+		fs.mkdirSync(dst_dir);
 	}
 	catch (e) {
 		if (e.code !== 'EEXIST') {
@@ -209,11 +266,11 @@ function createPage(cate_tag, total_page, page_num) {
 
         //添加继续阅读
 		article_content += "<br/>";
-		article_content += "<p><a title=\"\" class=\"btn btn-primary pull-left\" href=\"/detail/" + file + ".html\">继续阅读  →</a> </p><br/> <br/>";
+		article_content += "<p><a title=\"\" class=\"btn btn-primary pull-left\" href=\"/" + article_dir + '/' + file + ".html\">继续阅读  →</a> </p><br/> <br/>";
 		article_content += "<div class=\"line_dashed\"></div>";
 	}
 	
-	var selector = '.navbar-inner .nav li a[href="/' + cate_tag + '/page-1.html"]';
+	var selector = '.navbar-inner .nav li a[href="/' + cate_prefix + cate_tag + '/page-1.html"]';
 	var element = document.querySelectorAll(selector)[0];
 	element.parentNode.className = "active";
 	//this.$('.navbar-inner .nav li a[href="#cate/' + this.cate + '"]').parent().addClass('active');
@@ -234,13 +291,13 @@ function createPage(cate_tag, total_page, page_num) {
 	del.parentNode.removeChild(del);
 	del = document.getElementById('tpl-pagebar');
 	del.parentNode.removeChild(del);
-	fs.writeFileSync(dest_path, document.innerHTML);
-	console.log('写静态页面：' + dest_path + " 完成");
+	fs.writeFileSync(dst_path, document.innerHTML);
+	console.log('写静态页面：' + dst_path + " 完成");
 }
 
 var index_load_timer = setInterval(function () {
 	if ('undefined' !== typeof my_global.index.template) {
-		//已经加载首页模板并且解析完毕
+		// 已经加载首页模板并且解析完毕
 		clearInterval(index_load_timer);
 		
 		var cates = my_global.cates;
@@ -251,8 +308,22 @@ var index_load_timer = setInterval(function () {
 				var total_file_count = cate_artices.length;
 				var total_page = Math.floor((total_file_count + page_size - 1) / page_size);
 				for (var page_num = 1; page_num <= total_page; page_num++) {
-					createPage(cate_tag, total_page, page_num);
+					createPageView(cate_tag, total_page, page_num);
 				}
+			}
+		}
+		
+		//复制首页
+		var index_src_file = proj_path + '/' + cate_prefix + index_cate_tag + '/page-1.html';
+		var index_dst_file = proj_path + '/index.html';
+		var index_src_html = fs.readFileSync(index_src_file);
+		fs.writeFileSync(index_dst_file, index_src_html);
+		
+		// 写具体页
+		var articles = my_global.article;
+		for (var article in articles) {
+			if (articles.hasOwnProperty(article)) {
+				createArticleView(article, articles[article]);
 			}
 		}
 	}
